92ebca486f5a33d2e7aef2b45e2d9a647df2b675
DAVID <DAVID@DAVID-PC>
2015-05-20 23:10:10 +0200

    se modifica la clase conexion a la base de datos

--- examen1/src/modelo/ConexionDB.java
+++ examen1/src/modelo/ConexionDB.java
@@ -4,18 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- * Clase de utilidad para conectar con la Base de Datos.
- * 
- * @author Sergio
- *
- */
 public class ConexionDB {
 				
 	private String user;   //Guarda el nombre del usuario con que nos conectaremos.
 	private String bbdd;  //Guarda el nombre de la bd a la que nos conectamos.
 	private String url;  // Guarda la direccion de la bd.
-	private String pass;// Gurda el pass con el que accedemos.
+	private String pass;// Guarda el pass con el que accedemos.
 	
 	private static Connection con; //Describe una conexión (puente entre App Java y BD) 
 	
